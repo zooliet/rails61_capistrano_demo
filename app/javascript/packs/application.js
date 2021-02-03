5e7e1dc434eb193481fcefb708e935d8b16022e3
Junhyun Shin <hl1sqi@gmail.com>
2021-02-03 23:06:09 +0900

    first commit

---
+++ app/javascript/packs/application.js
@@ -0,0 +1,23 @@
+// This file is automatically compiled by Webpack, along with any other files
+// present in this directory. You're encouraged to place your actual application logic in
+// a relevant structure within app/javascript and only use these pack files to reference
+// that code so it'll be compiled.
+
+import Rails from "@rails/ujs"
+import "@hotwired/turbo-rails"
+import * as ActiveStorage from "@rails/activestorage"
+import "channels"
+
+Rails.start()
+ActiveStorage.start()
+
+import "controllers"
+import 'jquery'
+import 'bootstrap'
+import 'bootstrap/dist/css/bootstrap.css'
+import 'data-confirm-modal'
+
+document.addEventListener('turbo:load', () => {
+  jQuery('[data-toggle="tooltip"]').tooltip()
+  jQuery('[data-toggle="popover"]').popover()
+})
